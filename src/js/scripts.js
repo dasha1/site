6d8a1507ebce6042a3d20b14893d5ff79f126399
dasha1 <d.o.perfilieva@gmail.com>
2016-09-13 21:16:19 +0300

    animation added

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,67 +3,75 @@ $(document).ready( function () {
             $('#navbar-toggle').toggleClass('active');
             $('.menu').toggleClass('menu-right');
         });
+        $('.skils-content').viewportChecker({
+            callbackFunction: function() {
 
+            }
+            }
+
+    );
     }
 );
 
 
+$('.skills-content').on('appear', function() {
+    $('.circle.first').circleProgress({
+        value: 0.95,
+        size: 200,
+        fill: {
+            color: ["#b288ab"]
+        }
+    });
+    $('.circle.second').circleProgress({
+        value: 0.75,
+        size: 200,
+        fill: {
+            color: ["#b288ab"]
+        }
+    });
+    $('.circle.third').circleProgress({
+        value: 0.85,
+        size: 200,
+        fill: {
+            color: ["#b288ab"]
+        }
+    });
+    $('.circle.four').circleProgress({
+        value: 0.75,
+        size: 200,
+        fill: {
+            color: ["#b288ab"]
+        }
+    });
+    $('.circle.five').circleProgress({
+        value: 0.65,
+        size: 200,
+        fill: {
+            color: ["#b288ab"]
+        }
+    });
+    $('.circle.six').circleProgress({
+        value: 0.55,
+        size: 200,
+        fill: {
+            color: ["#b288ab"]
+        }
+    });
+    $('.circle.seven').circleProgress({
+        value: 0.45,
+        size: 200,
+        fill: {
+            color: ["#b288ab"]
+        }
+    });
+    $('.circle.eight').circleProgress({
+        value: 0.45,
+        size: 200,
+        fill: {
+            color: ["#b288ab"]
+        }
+    });
+});
 
-$('.circle.first').circleProgress({
-    value: 0.95,
-    size: 200,
-    fill: {
-        color: ["#b288ab"]
-    }
-});
-$('.circle.second').circleProgress({
-    value: 0.75,
-    size: 200,
-    fill: {
-        color: ["#b288ab"]
-    }
-});
-$('.circle.third').circleProgress({
-    value: 0.85,
-    size: 200,
-    fill: {
-        color: ["#b288ab"]
-    }
-});
-$('.circle.four').circleProgress({
-    value: 0.75,
-    size: 200,
-    fill: {
-        color: ["#b288ab"]
-    }
-});
-$('.circle.five').circleProgress({
-    value: 0.65,
-    size: 200,
-    fill: {
-        color: ["#b288ab"]
-    }
-});
-$('.circle.six').circleProgress({
-    value: 0.55,
-    size: 200,
-    fill: {
-        color: ["#b288ab"]
-    }
-});
-$('.circle.seven').circleProgress({
-    value: 0.45,
-    size: 200,
-    fill: {
-        color: ["#b288ab"]
-    }
-});
-$('.circle.eight').circleProgress({
-    value: 0.45,
-    size: 200,
-    fill: {
-        color: ["#b288ab"]
-    }
-});
 
 
